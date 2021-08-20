422d06f775c8bdc3fce8c1a1b0a5d7030a552f4a
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-08-20 20:53:01 -0300

    Scafold CRUD

---
+++ WebAp/Data/WebApContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAp.Models;
+
+namespace WebAp.Data
+{
+    public class WebApContext : DbContext
+    {
+        public WebApContext (DbContextOptions<WebApContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAp.Models.Department> Department { get; set; }
+    }
+}
