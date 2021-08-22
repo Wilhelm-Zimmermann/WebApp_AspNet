a42969c240fa7a99a8e5e8436a452365ebc33416
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-08-22 17:34:25 -0300

    Post form added

--- WebAp/Controllers/SellersController.cs
+++ WebAp/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAp.Services;
+using WebAp.Models;
 
 namespace WebAp.Controllers
 {
@@ -22,6 +23,18 @@ namespace WebAp.Controllers
             var list = _sellerService.FindAll();
             
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
