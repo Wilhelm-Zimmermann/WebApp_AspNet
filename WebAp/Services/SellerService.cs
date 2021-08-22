c824af9eb11c250eb82a94f5cc5b303d57f588b6
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-08-22 19:24:14 -0300

    Details And Delete

--- WebAp/Services/SellerService.cs
+++ WebAp/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebAp.Models;
 using WebAp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAp.Services
 {
@@ -24,6 +25,18 @@ namespace WebAp.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
